4537effd5d99eafc3d3a38ada2c07960e1e568ba
caiol <caiolvbarbieri@pm.me>
2022-01-07 22:52:49 -0300

    implemented GET & POST methods

---
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -0,0 +1,32 @@
+package com.products.apirest.resources;
+
+import com.products.apirest.models.Product;
+import com.products.apirest.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProductResource {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public List<Product> productList() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product product(@PathVariable(value="id") long id) {
+        return productRepository.findById(id);
+    }
+
+    @PostMapping("/product")
+    public Product saveProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+}
