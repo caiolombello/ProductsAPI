31cd35e78c5e0df767c4718c2463e951f066bb5b
caiol <caiolvbarbieri@pm.me>
2022-01-08 01:35:30 -0300

    swagger fix but need config path /swagger-ui/index.html

--- src/main/java/com/products/apirest/config/SwaggerConfig.java
+++ src/main/java/com/products/apirest/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.products.apirest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,8 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -23,14 +24,14 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.products.apirest"))
-                .paths(regex("/api.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaInfo());
     }
 
     private ApiInfo metaInfo() {
-        ApiInfo apiInfo = new ApiInfo(
+        return new ApiInfo(
                 "Products API REST",
                 "API REST product registration.",
                 "1.0",
@@ -39,6 +40,6 @@ public class SwaggerConfig {
                 "Apache License Version 2.0",
                 "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
         );
-        return apiInfo;
     }
+
 }
